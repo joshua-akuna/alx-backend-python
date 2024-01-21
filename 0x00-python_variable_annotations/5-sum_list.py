56aa7cf1267951106f7031ec7feb64ab5ec3b195
Akuna Joshua <akunajoshua@gmail.com>
2024-01-21 16:18:15 +0100

    5-sum_list.py file commit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines the sum_list function
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of all the arguments in the list floats"""
+    return sum(input_list)

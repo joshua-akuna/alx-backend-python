9e6174633a625fae55c4ce029d8d62327b328ec9
Akuna Joshua <akunajoshua@gmail.com>
2024-01-21 14:28:15 +0100

    3-to_str.py file commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines the to_str function"""
+
+
+def to_str(n: float) -> str:
+    """
+    the function takes the float argument and returns its]
+    string representation
+    """
+    return str(n)

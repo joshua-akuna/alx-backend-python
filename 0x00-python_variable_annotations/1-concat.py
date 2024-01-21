61b8427ab8d9e7732773fbe082bb4f75790b9059
Akuna Joshua <akunajoshua@gmail.com>
2024-01-21 13:52:18 +0100

    1-concat.py file commit

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""The module defines the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates 2 strings and returns the result"""
+    return "{}{}".format(str1, str2)

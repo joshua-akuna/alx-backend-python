77785c940a21be2d2b362b67d0f4c1566cae4f7e
Akuna Joshua <akunajoshua@gmail.com>
2024-01-21 13:41:02 +0100

    0-add.py : correction : pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """The module defines a function that adds 2 floats and returns a float"""
 
-def add (a: float, b: float) -> float:
+
+def add(a: float, b: float) -> float:
     return a + b
